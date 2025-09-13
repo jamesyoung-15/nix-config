81174f7f90827aa6187a08c16b3d22baeb185bfe
James Young <jyyoung@jyymail.com>
2025-09-13 00:21:51 -0400

    update flatpak scripts

--- scripts/flatpak-export.sh
+++ scripts/flatpak-export.sh
@@ -1,1 +1,1 @@
-flatpak list --app --columns=origin --columns=application | awk '{print "flatpak install " $1,$2 " -y"}' > install-flatpaks.sh
+flatpak list --app --columns=origin --columns=application | awk '{print "flatpak install " $1,$2 " -y"}' > ./scripts/install-flatpaks.sh
--- scripts/flatpak-setup.sh
+++ scripts/flatpak-setup.sh
@@ -1,3 +1,4 @@
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.google.Chrome -y
 flatpak install flathub com.logseq.Logseq -y
 flatpak install flathub io.github.zaps166.QMPlay2 -y
