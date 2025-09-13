6dfee91b973ff1137ffb4706703f1614e081ae3a
James Young <jyyoung@jyymail.com>
2025-09-13 00:50:54 -0400

    add cursor and icon themes

--- modules/plasma/themes.nix
+++ modules/plasma/themes.nix
@@ -16,5 +16,14 @@
     pkgs.dracula-theme
     pkgs.lightly-qt
     kdePackages.sddm-kcm # sddm theme
+
+    # cursors, icons, etc.
+    pkgs.catppuccin-cursors
+    pkgs.papirus-icon-theme
+    (pkgs.tela-circle-icon-theme.override { colorVariants = ["dracula" "purple"]; })
+    pkgs.dracula-icon-theme
+    # (pkgs.whitesur-icon-theme.override { themeVariants = ["purple" "nord"];})
+    # paper-icon-theme
+    pkgs.capitaine-cursors
   ];
 }
