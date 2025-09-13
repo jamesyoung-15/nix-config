f3cee8cc50c20cf092ecc9cfc29a37ef07b5b766
James Young <jyyoung@jyymail.com>
2025-09-12 23:32:44 -0400

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "NixOS configuration by James Young";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-hardware,
+      home-manager,
+      ...
+    }:
+    {
+      nixosConfigurations = {
+        main-desktop = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/main-desktop
+
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.jamesyoung = ./home-manager/home.nix;
+
+              home-manager.extraSpecialArgs = {
+                username = "jamesyoung";
+              };
+            }
+          ];
+        };
+        framework-13 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/framework-13
+            nixos-hardware.nixOsModules.framework-13-7040-amd
+
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.jamesyoung = ./home-manager/home.nix;
+
+              home-manager.extraSpecialArgs = {
+                username = "jamesyoung";
+              };
+            }
+          ];
+        };
+        vm-test = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/vm-test
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.james = ./home-manager/home.nix;
+
+              home-manager.extraSpecialArgs = {
+                username = "james";
+              };
+            }
+          ];
+        };
+      };
+    };
+}
