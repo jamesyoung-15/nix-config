4500104edf35ba3a7fec27140374b54260da192f
James Young <jyyoung@jyymail.com>
2025-09-17 10:25:35 -0400

    update python nix shell

--- nix-shells/python.nix
+++ nix-shells/python.nix
@@ -3,11 +3,16 @@
 }:
 
 let
-  pythonEnv = pkgs.python3.withPackages (ps: [ ]);
+  pythonEnv = pkgs.python313.withPackages (ps: [ ]);
 
 in
 pkgs.mkShell {
   packages = [
     pythonEnv
+  ];
+
+  env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+    pkgs.libz
+    pkgs.stdenv.cc.cc.lib
   ];
 }
