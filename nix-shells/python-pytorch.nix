5f05ed8f15d981f01a837bb366cb8fabeefbfa06
James Young <jyyoung@jyymail.com>
2025-11-14 17:26:15 -0500

    add nix shell rocm pytorch support

--- nix-shells/python-pytorch.nix
+++ nix-shells/python-pytorch.nix
@@ -3,14 +3,20 @@
 }:
 
 let
-  pythonEnv = pkgs.python312.withPackages (ps: [ 
-    ps.torch
+  pythonEnv = pkgs.python312.withPackages (ps: [
+    ps.torchWithRocm
+    (ps.torchvision.override { torch = ps.torchWithRocm; })
+    (ps.torchaudio.override { torch = ps.torchWithRocm; })
+    ps.scipy
+    ps.pandas
+    ps.matplotlib
   ]);
 
 in
 pkgs.mkShell {
   packages = [
     pythonEnv
+    pkgs.rocmPackages.clr
   ];
 
   env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
