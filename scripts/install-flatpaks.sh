3fa51affd5ee321e65075710b8c7b54fc3507d51
James Young <jyyoung@jyymail.com>
2025-11-15 00:35:41 -0500

    setup flatpaks install script

---
+++ scripts/install-flatpaks.sh
@@ -0,0 +1,10 @@
+flatpak install flathub com.google.Chrome -y
+flatpak install flathub com.logseq.Logseq -y
+flatpak install flathub com.usebruno.Bruno -y
+flatpak install flathub io.github.zaps166.QMPlay2 -y
+flatpak install flathub net.cozic.joplin_desktop -y
+flatpak install flathub org.onlyoffice.desktopeditors -y
+flatpak install flathub org.qbittorrent.qBittorrent -y
+flatpak install flathub org.raspberrypi.rpi-imager -y
+flatpak install flathub org.ryujinx.Ryujinx -y
+flatpak install flathub org.upscayl.Upscayl -y
