7c7bf0e30dce9426db6ef06f2185c49b4abe6679
James Young <jyyoung@jyymail.com>
2026-05-17 00:53:04 -0400

    remove unused tools

--- modules/dev/tools.nix
+++ modules/dev/tools.nix
@@ -3,13 +3,11 @@
   environment.systemPackages = with pkgs; [
     # dev tools
     jupyter
-    hugo
     gh
     awscli2
     google-cloud-sdk
     act # local github actions
     localstack # local aws stack
-    ollama
     pre-commit
 
     # db
